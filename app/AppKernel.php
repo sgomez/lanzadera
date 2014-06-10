0f2cde4a9e60d6b58d8de78942a837d47b6a6a08
Sergio Gómez <sergio@uco.es>
2014-06-10 15:28:53 +0200

    Pasamos de usar el framework CSS Gumby a Bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
             new Tejedora\LanzaderaBundle\LanzaderaBundle(),
         );
 

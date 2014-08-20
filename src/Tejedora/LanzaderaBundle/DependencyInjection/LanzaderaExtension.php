944fad3b0221a51e24c7046d31201b269331aff3
Sergio Gómez <sergio@uco.es>
2014-08-20 23:43:43 +0200

    Bundle TejedoraLanzaderaBundle obsoleto
    Este bundle está obsoleto y poco a poco se moverá lo que hay al resto
    de bundles

--- src/Tejedora/LanzaderaBundle/DependencyInjection/LanzaderaExtension.php
+++ src/Tejedora/LanzaderaBundle/DependencyInjection/LanzaderaExtension.php
@@ -24,6 +24,5 @@ class LanzaderaExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-        $loader->load('admin.xml');
     }
 }

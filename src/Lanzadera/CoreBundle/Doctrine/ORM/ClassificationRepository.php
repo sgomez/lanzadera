76d153960a66bb1cfca8f11aa00ebf23438d5c02
Sergio Gómez <sergio@uco.es>
2014-08-20 23:46:10 +0200

    Creados nuevos repositorios como servicios
    Se crean:
    - ClassificationRepository
    - OrganizationRepository

---
+++ src/Lanzadera/CoreBundle/Doctrine/ORM/ClassificationRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergio
+ * Date: 20/08/14
+ * Time: 07:37
+ */
+
+namespace Lanzadera\CoreBundle\Doctrine\ORM;
+
+
+class ClassificationRepository extends CustomRepository
+{
+}

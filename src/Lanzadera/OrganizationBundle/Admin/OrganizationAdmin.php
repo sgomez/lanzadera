4b84f2b2066fc4fc06c1d495f3ae3834fc694a49
Sergio Gómez <sergio@uco.es>
2014-08-20 23:39:49 +0200

    Se crea la versión preliminar de la gestión de Organizaciones

---
+++ src/Lanzadera/OrganizationBundle/Admin/OrganizationAdmin.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergio
+ * Date: 10/06/14
+ * Time: 17:14
+ */
+
+namespace Lanzadera\OrganizationBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+/**
+ * Class OrganizationAdmin
+ * @package Tejedora\Lanzadera\CoreBundle\Admin
+ */
+
+class OrganizationAdmin extends Admin
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('phone')
+            ->add('email')
+            ->add('enabled', null, array('editable' => true))
+            ->add('createdAt', 'date', array(
+                    'pattern' => 'dd MMMM Y',
+                    'locale' => 'es',
+                    'timezone' => 'Europe/Madrid',
+            ))
+            ->add('_action', 'actions', array(
+                    'actions' => array(
+                        'show' => array(),
+                        'edit' => array(),
+                        'delete' => array(),
+                    )
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General')
+                ->add('name')
+                ->add('address', 'textarea', array('required' => false))
+                ->add('phone')
+                ->add('email', 'email', array('required' => false))
+                ->add('web', 'url', array('required' => false))
+                ->add('enabled', 'checkbox', array('required' => false))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('address')
+            ->add('enabled')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('name')
+            ->add('address')
+            ->add('phone')
+            ->add('email', 'email')
+            ->add('web', 'url')
+            ->add('enabled')
+            ->add('created_at', 'date')
+        ;
+    }
+
+}
---
+++ src/Lanzadera/OrganizationBundle/Entity/Organization.php
@@ -0,0 +1,300 @@
+<?php
+
+namespace Lanzadera\OrganizationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Organization
+ *
+ * @ORM\Table(name="organization")
+ * @ORM\Entity(repositoryClass="Lanzadera\CoreBundle\Doctrine\ORM\OrganizationRepository")
+ */
+class Organization
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=255, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="web", type="string", length=255, nullable=true)
+     */
+    private $web;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="enabled", type="boolean", nullable=false)
+     */
+    private $enabled;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=false)
+     */
+    private $createdAt;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\ManyToMany(targetEntity="Lanzadera\ClassificationBundle\Entity\Parameter", inversedBy="organization")
+     * @ORM\JoinTable(name="organization_has_parameter",
+     *   joinColumns={
+     *     @ORM\JoinColumn(name="organization_id", referencedColumnName="id")
+     *   },
+     *   inverseJoinColumns={
+     *     @ORM\JoinColumn(name="parameter_id", referencedColumnName="id")
+     *   }
+     * )
+     */
+    private $parameter;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->parameter = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Organization
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Organization
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     * @return Organization
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string 
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Organization
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set web
+     *
+     * @param string $web
+     * @return Organization
+     */
+    public function setWeb($web)
+    {
+        $this->web = $web;
+
+        return $this;
+    }
+
+    /**
+     * Get web
+     *
+     * @return string 
+     */
+    public function getWeb()
+    {
+        return $this->web;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param string $enabled
+     * @return Organization
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return string 
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Organization
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Add parameter
+     *
+     * @param \Lanzadera\ClassificationBundle\Entity\Parameter $parameter
+     * @return Organization
+     */
+    public function addParameter(\Lanzadera\ClassificationBundle\Entity\Parameter $parameter)
+    {
+        $this->parameter[] = $parameter;
+
+        return $this;
+    }
+
+    /**
+     * Remove parameter
+     *
+     * @param \Lanzadera\ClassificationBundle\Entity\Parameter $parameter
+     */
+    public function removeParameter(\Lanzadera\ClassificationBundle\Entity\Parameter $parameter)
+    {
+        $this->parameter->removeElement($parameter);
+    }
+
+    /**
+     * Get parameter
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getParameter()
+    {
+        return $this->parameter;
+    }
+}

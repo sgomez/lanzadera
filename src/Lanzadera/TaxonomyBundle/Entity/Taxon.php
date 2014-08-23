7db1fbc94695902df0df73dd380b3205cb6801f5
Sergio Gómez <sergio@uco.es>
2014-08-23 07:07:08 +0200

    Creación del Bundle para las Taxonomías

---
+++ src/Lanzadera/TaxonomyBundle/Entity/Taxon.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergio
+ * Date: 22/08/14
+ * Time: 10:16
+ */
+
+namespace Lanzadera\TaxonomyBundle\Entity;
+
+use Sylius\Bundle\TaxonomiesBundle\Model\Taxon as SyliusTaxon;
+use Sylius\Bundle\TaxonomiesBundle\Model\TaxonInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Taxon
+ * @package Lanzadera\ClassificationBundle\Entity
+ * @ORM\MappedSuperclass()
+ */
+class Taxon extends SyliusTaxon
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct()
+    {
+       parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        return $this->getTabName();
+    }
+
+    /**
+     * Get tabbed name
+     *
+     * @return string
+     */
+    public function getTabName()
+    {
+        return str_repeat("—", $this->level) . $this->getName();
+    }
+}
---
+++ src/Lanzadera/TaxonomyBundle/LanzaderaTaxonomyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lanzadera\TaxonomyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LanzaderaTaxonomyBundle extends Bundle
+{
+}
